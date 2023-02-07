1954d6c22c3fc4881515f2db9809d15404361e1f
Nick Story <storynickolasl@gmail.com>
2023-02-06 19:38:23 -0500

    Initial Migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+ 
+  # Review.create(review: Faker::Creature::Animal.name)
+end
+
+# Beer.create(name: Faker::Creature::Animal.name, abv: Faker::Beer.alcohol, beer_style_id: 1)
+# Brewery.create(name: (Faker::Creature::Animal.name + ' ' Faker::Creature::Animal.name) )
+
+
