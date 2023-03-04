a5ceaf11a4e1170a35ce49dd37f712d75c649c14
Nick Story <storynickolasl@gmail.com>
2023-03-04 09:14:11 -0500

    Review Delete Function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,37 @@
 class ReviewsController < ApplicationController
+  skip_before_action :authorized
+
+    def show
+      review = Review.where(:id => params[:id])
+      render json: review
+    end
 
     #GET /Review
     def index
       render json: Review.all
     end
+
+    def create
+      review = Review.create(review_params)
+      render json: review, status: :created
+    end
+
+    def destroy
+      review = Review.find_by(id: params[:id])
+      # if user
+        review.destroy
+        head :no_content
+      # else
+      #   render_not_found_response
+      # end
+    end
+
+    private
+    # all methods below here are private
+  
+    def review_params
+      params.permit(:beer_id, :user_id, :review, :rating)
+    end
+  
 
 end
--- client/src/Beer.js
+++ client/src/Beer.js
@@ -7,7 +7,7 @@ import { Rating } from 'semantic-ui-react'
 function Beer({special}) {
   const [newName, setNewName] = useState('')
   const [form, setForm] = useState(true)
-  const [rate, setRate] = useState(true)
+  const [rate, setRate] = useState(3)
 
   const { beers } = useSelector((state) => state.brew)
 
@@ -29,14 +29,37 @@ function Beer({special}) {
     setForm(false)
   }
 
-  function handleSubmit() {
-    let cow = {
+  function handleSubmit(e) {
+    e.preventDefault();
+    let formData = {
+      user_id: 1,
       beer_id: special.id,
       rating: rate,
       review: newName
     }
-    console.log(cow)
-    setForm(true)
+    fetch("http://localhost:4000/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((review) => {
+        setForm(true)
+        console.log(review);
+      });
+  }
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/reviews/${id}`, {
+      method: "DELETE",
+    }).then((r) => {
+      if (r.ok) {
+        console.log('test');
+      }
+    });
   }
 
   return (
@@ -58,39 +81,39 @@ function Beer({special}) {
             </div>
           </Card>
             {special.reviews.length > 0 ? special.reviews.map((item) => 
-                    <Card><div className='test2' key={item.id}>
-                      <h3>{item.review}</h3>
-                    </div></Card>) : ''}
-                     {form ?
-                    <Card>
-                     
-                      <Button 
-                      color='black'
-                      onClick={handleAdd}
-                      >Add A Review</Button>
-                    </Card>
-                    : 
-                    <Card>
-                      <h4>Your Review</h4>
-                      <div>
-                        <label>Review: </label>
-                      <input defaultValue={'test'} onChange={handleName}/> 
-                      </div>
-                      <div>
-                         <label>Rating: </label>
-                    
-                      <Rating icon='star' defaultRating={3} maxRating={5} onRate={handleRating} />
-                     
-                      </div>
-                      <Button 
-                      color='black'
-                      onClick={handleSubmit}
-                      >Add Review</Button>
-                    </Card>
-                    }
+              <Card>
+                <div className='test2' key={item.id}>
+                  <h3>{item.rating}/5</h3>
+                  <h3>{item.review}</h3>
+                  <button onClick={() => handleDelete(item.id)}>Delete</button>
+                </div>
+          </Card>) : ''}
+          {form ? <Card>
+            <Button 
+              color='black'
+              onClick={handleAdd}
+              >Add A Review
+            </Button>
+            </Card>
+            : 
+            <Card>
+              <h4>Your Review</h4>
+              <div>
+                <label>Review: </label>
+              <input defaultValue={'test'} onChange={handleName}/> 
+              </div>
+              <div>
+                <label>Rating: </label>
+                <Rating icon='star' defaultRating={3} maxRating={5} onRate={handleRating} />
+              </div>
+                <Button 
+                color='black'
+                onClick={handleSubmit}
+                >Add Review
+                </Button>
+            </Card>}
           </Card.Group>
             <Link to={`/beers`} style={{ color: 'white' }}>
-              
               <Button
                 color='black'
                 >Return to Beer List
